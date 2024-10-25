b23d5668d8b0870225e449eed7a32c37484a66ac
francielle_sales <francielle_sales@hotmail.com>
2024-10-25 00:21:12 -0300

    create init.sql

---
+++ database/sql/init.sql
@@ -0,0 +1,33 @@
+-- init.sql
+
+-- Creating the user table
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create images table
+CREATE TABLE IF NOT EXISTS images (
+    id SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL UNIQUE,
+    "path" VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create results table
+CREATE TABLE IF NOT EXISTS results (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    image_id INT NOT NULL,
+    x_min DOUBLE PRECISION NOT NULL,
+    y_min DOUBLE PRECISION NOT NULL,
+    x_max DOUBLE PRECISION NOT NULL,
+    y_max DOUBLE PRECISION NOT NULL,
+    license_plate VARCHAR(7) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (image_id) REFERENCES images(id) ON DELETE CASCADE
+);
