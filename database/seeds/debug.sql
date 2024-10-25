bad641a722dd8cc2b0988fae28f2550c18f4383b
francielle_sales <francielle_sales@hotmail.com>
2024-10-25 00:23:33 -0300

    create seeds.sql

---
+++ database/seeds/debug.sql
@@ -0,0 +1,8 @@
+-- seeds.sql
+
+-- Inserção de dados na tabela de usuários
+INSERT INTO users ("name", email, "password")
+VALUES 
+    ('usuario1', 'usuario1@example.com', 'senha123'),
+    ('usuario2', 'usuario2@example.com', 'senha456'),
+    ('usuario3', 'usuario3@example.com', 'senha789');
