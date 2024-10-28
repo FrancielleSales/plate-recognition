da5f27736a81daf4851e1a65ca537393195d1489
francielle_sales <francielle_sales@hotmail.com>
2024-10-28 20:38:51 -0300

    add frontend configs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,17 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:8000"
+});
 
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.provide("axios", axiosInstance);
+app.mount("#app");
