495b4a9fa5c1f3e71c4789f02ecbc47c4c02cc1b
francielle_sales <francielle_sales@hotmail.com>
2024-11-09 21:13:05 -0300

    improve Dropzone component

--- frontend/src/components/DropZone.vue
+++ frontend/src/components/DropZone.vue
@@ -14,22 +14,25 @@ const emit = defineEmits(["upload"]);
 const fileInput = ref(null);
 const isDragging = ref(false);
 
+// Variables to control the selected file
+const selectedFile = ref(null);
+
 // Function to trigger a click on the file input
 const triggerUpload = () => {
   fileInput.value.click();
 };
 
-// Function called when a file is dragged over the zone
+// Function called when a file is dragged over the drop zone
 const onDragOver = () => {
   isDragging.value = true;
 };
 
-// Function called when the file leaves the drag zone
+// Function called when the file leaves the drop zone
 const onDragLeave = () => {
   isDragging.value = false;
 };
 
-// Function called when a file is dropped in the zone
+// Function called when the file is dropped in the drop zone
 const onDrop = (event) => {
   isDragging.value = false;
   const files = event.dataTransfer.files;
@@ -41,6 +44,7 @@ const handleFiles = (event) => {
   const files = Array.from(event.target.files);
   const filteredFiles = files.filter((file) => isFileAccepted(file));
   if (filteredFiles.length) {
+    selectedFile.value = filteredFiles[0];
     emit("upload", filteredFiles);
   }
 };
@@ -55,14 +59,14 @@ const isFileAccepted = (file) => {
     .some((type) => file.type.startsWith(type.trim()));
 };
 
-// Computed property to define the accepted types
+// Computed property for accepted file types
 const acceptTypes = computed(() => {
   return props.allowedTypes ? props.allowedTypes : "*";
 });
 </script>
 
 <template>
-  <div class="container mt-5">
+  <div class="container mt-4">
     <div class="d-flex justify-content-center">
       <div
         class="border border-primary rounded p-5 text-center drop-zone"
@@ -73,7 +77,23 @@ const acceptTypes = computed(() => {
         :class="{ 'drag-over': isDragging }"
         style="max-width: 80%; width: 100%"
       >
-        <h6>Arraste e solte os arquivos, ou clique para selecionar.</h6>
+        <i
+          v-if="!selectedFile"
+          class="bi bi-upload custom-color custom-icon-size"
+        ></i>
+        <i
+          v-else
+          class="bi bi-file-earmark-check custom-color custom-icon-size"
+        ></i>
+
+        <h6 class="mt-2">
+          {{
+            selectedFile
+              ? "Arquivo selecionado: " + selectedFile.name
+              : "Arraste ou clique para selecionar um arquivo."
+          }}
+        </h6>
+
         <input
           type="file"
           ref="fileInput"
@@ -82,15 +102,39 @@ const acceptTypes = computed(() => {
           hidden
           @change="handleFiles"
         />
-        <button class="btn btn-primary mt-4" @click="triggerUpload">
+        <button
+          class="btn btn-primary mt-4"
+          v-show="!selectedFile"
+          @click="triggerUpload"
+        >
           Selecionar arquivos
         </button>
       </div>
     </div>
   </div>
+  <small
+    v-if="allowedTypes && !selectedFile"
+    class="text-center text-secondary"
+  >
+    Apenas arquivos do tipo {{ allowedTypes }} são aceitos.
+  </small>
 </template>
 
 <style scoped>
+.custom-color {
+  color: #0d6efd;
+}
+
+.custom-color:hover,
+.custom-color:focus,
+.custom-color:active {
+  color: #0b5ed7;
+}
+
+.custom-icon-size {
+  font-size: 4rem;
+}
+
 .drop-zone {
   transition: background-color 0.3s;
 }
@@ -101,5 +145,11 @@ const acceptTypes = computed(() => {
 
 .drop-zone.drag-over {
   background-color: rgba(13, 110, 253, 0.2);
+}
+
+small {
+  display: block;
+  font-size: 0.875rem;
+  margin-top: 10px;
 }
 </style>
